910b6c30ab6bd0af919c4991e4c4a144a923baf4
Zachary A. Keener <zakkeener@gmail.com>
2019-11-13 15:36:34 -0700

    Add emacs config

---
+++ emacs/.emacs.files/init.el
@@ -0,0 +1,29 @@
+;;; package --- Emacs config for prj2501.
+
+;;; Commentary:
+;;; Emacs config for prj2501 -- streamed live @ twitch.tv/prj2501.
+
+;;; Code:
+
+(add-to-list 'load-path "~/.emacs.files/init")
+
+(load "init-packages")
+(load "init-general")
+(load "init-multi-cursor")
+(load "init-flycheck")
+(load "init-go")
+
+;;; init.el ends here
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (multiple-cursors go-mode markdown-mode ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs/.emacs.files/init/init-packages.el
@@ -0,0 +1,43 @@
+;;; Initialize packages.
+;; custom-set-variables was added by Package.el.
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+;; Refresh package management.
+(package-initialize)
+
+;; List of packages we want to install.
+(defvar package-list)
+(setq package-list
+      '(
+	;; Major modes.
+	go-mode	
+        markdown-mode
+
+	;; Minor modes.
+	lsp-mode
+	multiple-cursors
+
+	;; Golang.
+	flycheck-golangci-lint
+        go-guru
+        go-rename
+
+	;; Utilities.
+	ag ;; Simple and quick searching.
+	company
+	company-lsp
+	flycheck
+	lsp-ui
+	))
+
+;; Install the missing packages.
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(provide 'init-packages)
+
+;;; End of package initialization.
